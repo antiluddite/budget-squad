4d0d1f9a1d60bfd92e22590f27cf96d97f60b2f0
Maureen O'Flynn <maureen.p.oflynn@gmail.com>
2022-04-27 17:48:07 -0700

    updatedTransactionTable

--- src/transactions/TransactionTable.js
+++ src/transactions/TransactionTable.js
@@ -2,21 +2,43 @@ import React from 'react';
 import { PropTypes } from 'prop-types';
 import { v4 as uuid } from 'uuid';
 import { useSelector } from 'react-redux';
+// import { propTypes } from 'react-bootstrap/esm/Image';
 import TransactionRow from './TransactionRow';
 import TransactionForm from './TransactionForm';
 
 /**
  * Shows a table of transactions and optionally a form for creating a transaction
- *
+*Added props to the TransactionTable component that allows filtering by:
+start date (inclusive),end date (inclusive), category, payee (name).
+
+// "filters" is prop name for now
+// The "filters" prop is optional,
+it may not be set at all, in which case the table should show everything.
+// "filters" prop may be missing or set to null, and those should be ignored.
+
  * @component
  * @example
  * <TransactionTable isCreating onStopCreating={() => alert('finished')} />
  */
-function TransactionTable({ isCreating = false, onStopCreating }) {
-  // Get all the transactions to show in the table
-  const transactionList = useSelector((state) => state.transactions.entities);
 
-  // JSX
+// added filters
+function TransactionTable({ isCreating = false, onStopCreating, filters }) {
+  // "filters" prop added.
+  const transactionList = useSelector((state) => state.transactions.entities.filter(
+    (t) => (t.categoryId === filters.categoryId
+    || filters.categoryId === null
+    || filters.categoryId === undefined)
+    && (t.payee === filters.payee
+    || filters.payee === null
+    || filters.payee === undefined)
+    && (t.date >= filters.startDate
+    || filters.startDate === null
+    || filters.startDate === undefined)
+    && (t.date <= filters.endDate
+    || filters.endDate === null
+    || filters.endDate === undefined),
+  ));
+
   return (
     <table className="table table-striped">
       <thead>
@@ -40,6 +62,13 @@ function TransactionTable({ isCreating = false, onStopCreating }) {
 }
 
 TransactionTable.propTypes = {
+  filters: PropTypes.exact({
+    categoryId: PropTypes.string,
+    payee: PropTypes.string,
+    startDate: PropTypes.number,
+    endDate: PropTypes.number,
+  }).isRequired,
+  // date: new Date(formData.date)
   /**
    * Is a transaction currently being created - shows the creating form
    */
